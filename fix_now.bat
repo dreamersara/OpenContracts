3b08baff7e89f26e79ddd021f4dddd8265e33eb0
dreamersara <sarakasim32@gmail.com>
2025-10-25 14:18:50 +0300

    Update fix_now.bat

--- fix_now.bat
+++ fix_now.bat
@@ -11,6 +11,7 @@ echo EMBEDDINGS_MICROSERVICE_URL=http://localhost:5002 >> .env
 echo DOCLING_PARSER_SERVICE_URL=http://localhost:5001 >> .env
 echo VECTOR_EMBEDDER_API_KEY=dummy >> .env
 echo CELERY_BROKER_URL=memory:// >> .env
+echo REDIS_URL=redis://localhost:6379/0 >> .env
 echo USE_AWS=False >> .env
 echo EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend >> .env
 echo ANTHROPIC_API_KEY= >> .env
